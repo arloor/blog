5dd8778fb6792764a5497f6c1292360dcecd3ba1
arloor <admin@arloor.com>
2018-12-31 19:24:07 +0800

    readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# 博客
+使用hugo生成静态博客
+
+## 先安装hugo 0.53 （需要支持 scss）
+
+## 部署
+```
+bash deploy.sh
+````
+这样就直接部署到了我的centos7服务器上
+
+访问[arloor博客](http://arloor.com)
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
+# 当前使用hugo 0.53(支持scss)
+dir=/home/x1/blog
+host=arloor.com
+
 # yum install httpd
-cd /home/x1/blog
+# systemctl enable httpd
+cd $dir
 echo "生成静态资源..."
 hugo
-ssh root@arloor.com "
+ssh root@$host "
 echo "stop httpd ...."
 systemctl  stop httpd
 echo "删除服务器的旧版本静态资源...."
 rm -rf /var/www/html/*
 "
 echo "上传新的静态资源...."
-scp -r ./public/* root@arloor.com:/var/www/html
+scp -r ./public/* root@$host:/var/www/html
 echo "重启httpd...."
-ssh root@arloor.com "systemctl  start httpd"
+ssh root@$host "systemctl  start httpd"
+echo  "部署完毕，请访问 http://"$host
