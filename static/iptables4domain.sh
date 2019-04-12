b938368e85a68d0887992110a507c524070f46d9
arloor <admin@arloor.com>
2019-04-12 21:40:49 +0800

    自动提交 @arloor 2019年 04月 12日 星期五 21:40:49 CST

--- static/iptables4domain.sh
+++ static/iptables4domain.sh
@@ -44,12 +44,9 @@ fi
 
 # 开启端口转发
 sed -n '/^net.ipv4.ip_forward=1/'p /etc/sysctl.conf | grep -q "net.ipv4.ip_forward=1"
-if [ $? -eq 0 ]; then
-    echo ""
-else
+if [ $? -ne 0 ]; then
     echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf && sysctl -p
 fi
-
 
 ## 获取本机地址
 local=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 | grep -Ev '(^127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.1[6-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.2[0-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.3[0-1]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^192\.168\.[0-9]{1,3}\.[0-9]{1,3}$)')
@@ -59,13 +56,6 @@ fi
 echo target-ip: $remote
 echo  local-ip: $local
 
-
-# iptables -F -t nat
-# ## 因为iptables 规则被清空了，所以重启docker服务，已重新设置iptables规则
-# service docker restart
-
-
-## 中转到nathosts
 iptables -t nat -A PREROUTING -p tcp --dport $localport -j DNAT --to-destination $remote:$port
 iptables -t nat -A PREROUTING -p udp --dport $localport -j DNAT --to-destination $remote:$port
 iptables -t nat -A POSTROUTING -p tcp -d $remote --dport $port -j SNAT --to-source $local
