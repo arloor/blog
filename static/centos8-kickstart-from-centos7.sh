a3ccb53b8af45b4fb9bcf91ae202954784e2797b
arloor <admin@arloor.com>
2020-04-11 17:21:15 +0800

    自动提交 @arloor 2020年04月11日 17:21:14

--- static/centos8-kickstart-from-centos7.sh
+++ static/centos8-kickstart-from-centos7.sh
@@ -241,7 +241,7 @@ firstboot --enable
 # Do not configure the X Window System
 skipx
 # System services
-services --enabled="chronyd"
+services --disabled=firewalld --enabled="chronyd"
 sshkey --username=root "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZQzKHfZLlFEdaRUjfSK4twhL0y7+v23Ko4EI1nl6E1/zYqloSZCH3WqQFLGA7gnFlqSAfEHgCdD/4Ubei5a49iG0KSPajS6uPkrB/eiirTaGbe8oRKv2ib4R7ndbwdlkcTBLYFxv8ScfFQv6zBVX3ywZtRCboTxDPSmmrNGb2nhPuFFwnbOX8McQO5N4IkeMVedUlC4w5//xxSU67i1i/7kZlpJxMTXywg8nLlTuysQrJHOSQvYHG9a6TbL/tOrh/zwVFbBS+kx7X1DIRoeC0jHlVJSSwSfw6ESrH9JW71cAvn6x6XjjpGdQZJZxpnR1NTiG4Q5Mog7lCNMJjPtwJ not@home"
 # System timezone
 timezone Asia/Shanghai --isUtc
