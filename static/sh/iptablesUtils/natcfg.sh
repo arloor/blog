2ce01279c621e828e52fa92d063a05a74eedfa33
arloor <admin@arloor.com>
2020-09-30 19:51:47 +0800

    自动提交 @arloor 2020年09月30日 19:51:47

--- static/sh/iptablesUtils/natcfg.sh
+++ static/sh/iptablesUtils/natcfg.sh
@@ -7,11 +7,15 @@ conf=$base/conf
 touch $conf
 
 # wget wget --no-check-certificate -qO natcfg.sh http://blog.arloor.com/sh/iptablesUtils/natcfg.sh && bash natcfg.sh
-echo -e "${red}用途${black}: 便捷的设置iptables端口转发"
-echo -e "${red}项目地址${black}: https://github.com/arloor/iptablesUtils"
-echo -e "${red}注意1${black}: 到域名的转发规则在添加后需要等待2分钟才会生效，且在机器重启后仍然有效"
-echo -e "${red}注意2${black}: 到IP的转发规则在重启后会失效，这是iptables的特性"
-echo
+
+    clear
+    echo -e "#############################################################"
+    echo -e "# ${red}用途${black}: 便捷的设置iptables端口转发                 #"
+    echo -e "# ${red}项目地址${black}: https://github.com/arloor/iptablesUtils    #"
+    echo -e "#############################################################"
+    echo
+
+
 setupService(){
     cat > /usr/local/bin/dnat.sh <<"AAAA"
 #! /bin/bash
