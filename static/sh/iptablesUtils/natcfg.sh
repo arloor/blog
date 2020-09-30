0e54baef3b75137de88fb0632fa795b8f6ba7fcb
arloor <admin@arloor.com>
2020-09-30 19:54:37 +0800

    自动提交 @arloor 2020年09月30日 19:54:37

--- static/sh/iptablesUtils/natcfg.sh
+++ static/sh/iptablesUtils/natcfg.sh
@@ -202,26 +202,6 @@ if [ "${localIP}" = "" ]; then
         localIP=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1|head -n 1 )
 fi
 
-rmIptablesNat(){
-    #删除旧的中转规则
-        local arr1=(`iptables -L PREROUTING -n -t nat --line-number |grep DNAT|grep "dpt:$1 "|sort -r|awk '{print $1,$3,$9}'|tr " " ":"|tr "\n" " "`)
-        for cell in ${arr1[@]}  # cell= 1:tcp:to:8.8.8.8:543
-        do
-            local arr2=(`echo $cell|tr ":" " "`)  #arr2=(1 tcp to 8.8.8.8 543)
-            local index=${arr2[0]}
-            local proto=${arr2[1]}
-            local targetIP=${arr2[3]}
-            local targetPort=${arr2[4]}
-            # echo 清除本机$localport端口到$targetIP:$targetPort的${proto}的PREROUTING转发规则[$index]
-            iptables -t nat  -D PREROUTING $index
-            # echo ==清除对应的POSTROUTING规则
-            local toRmIndexs=(`iptables -L POSTROUTING -n -t nat --line-number|grep SNAT|grep $targetIP|grep dpt:$targetPort|grep $proto|awk  '{print $1}'|sort -r|tr "\n" " "`)
-            for cell1 in ${toRmIndexs[@]}
-            do
-                iptables -t nat  -D POSTROUTING $cell1
-            done
-        done
-}
 
 addDnat(){
     local localport=
@@ -260,10 +240,6 @@ rmDnat(){
     local localport=
     echo -n "本地端口号:" ;read localport
     sed -i "/^$localport>.*/d" $conf
-
-    rmIptablesNat $localport
-    #删除临时文件  
-    rm -f $base/${1}IP  
     echo "done!"
 }
 
@@ -300,40 +276,6 @@ do
 done
 }
 
-addSnat(){
-    local localport=
-    local remoteport=
-    local remotehost=
-    echo -n "本地端口号:" ;read localport
-    echo -n "远程端口号:" ;read remoteport
-    # echo $localport $remoteport
-    # 判断端口是否为数字
-    echo "$localport"|[ -n "`sed -n '/^[0-9][0-9]*$/p'`" ] && echo $remoteport |[ -n "`sed -n '/^[0-9][0-9]*$/p'`" ]||{
-        echo  -e "${red}本地端口和目标端口请输入数字！！${black}"
-        return 1;
-    }
-
-    echo -n "目标IP:" ;read remotehost
-    # 检查输入的不是IP
-    if [ "$remotehost" = "" -o "$(echo  $remotehost |grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}')" != "" ];then
-        rmIptablesNat $localport
-
-        ## 建立新的中转规则
-        iptables -t nat -A PREROUTING -p tcp --dport $localport -j DNAT --to-destination $remotehost:$remoteport
-        iptables -t nat -A PREROUTING -p udp --dport $localport -j DNAT --to-destination $remotehost:$remoteport
-        iptables -t nat -A POSTROUTING -p tcp -d $remotehost --dport $remoteport -j SNAT --to-source $localIP
-        iptables -t nat -A POSTROUTING -p udp -d $remotehost --dport $remoteport -j SNAT --to-source $localIP
-    else
-        echo 请输入一个IP
-        return 1
-    fi    
-}
-
-rmSnat(){
-    local localport=
-    echo -n "本地端口号:" ;read localport
-    echo "$localport"|[ -n "`sed -n '/^[0-9][0-9]*$/p'`" ] &&rmIptablesNat $localport
-}
 
 
 
