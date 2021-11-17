ec2d76aec83146988a379e89c52286aeb9c24023
arloor <admin@arloor.com>
2021-11-17 20:13:53 +0800

    commit @arloor 2021年11月17日 星期三 20时13分53秒 CST

--- deploy.sh
+++ deploy.sh
@@ -23,18 +23,21 @@ then
     rm -rf /var/blog
     wget -O /usr/local/bin/tarloor https://raw.githubusercontent.com/arloor/arloor.github.io/master/docs/tarloor_deb.sh
     bash tarloor 0 # 0不使用代理，1使用代理
+    service proxy restart
     "
 
     ssh root@hk.gcall.me  -p22 -t "
     rm -rf /var/blog
     wget -O /usr/local/bin/tarloor https://raw.githubusercontent.com/arloor/arloor.github.io/master/docs/tarloor.sh
     bash tarloor 0 # 0不使用代理，1使用代理
+    service proxy restart
     "
 
     ssh root@bwg.arloor.com  -p22 -t "
     rm -rf /var/blog
     wget -O /usr/local/bin/tarloor https://raw.githubusercontent.com/arloor/arloor.github.io/master/docs/tarloor.sh
     bash tarloor 0 # 0不使用代理，1使用代理
+    service proxy restart
     "
 echo -e "\033[32m 请访问： https://"$host"\033[0m"
 else
