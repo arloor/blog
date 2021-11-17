4eb64ccdcd9dcda3ba8da9c4346cdcc9a1ceda3d
arloor <admin@arloor.com>
2021-11-17 20:48:27 +0800

    commit @arloor Wed Nov 17 20:48:27 CST 2021

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ then
     # 该脚本会检查httpd、hugo、和git仓库，实现完全自动化
      ssh root@$host  -p$port -t "
      rm -rf /var/blog
-     wget -O /usr/local/bin/tarloor https://raw.githubusercontent.com/arloor/blog/master/static/tarloor.sh
+     wget -O /usr/local/bin/tarloor https://raw.githubusercontent.com/arloor/blog/master/static/tarloor_deb.sh
      bash tarloor 0 # 0不使用代理，1使用代理
      "
 echo -e "\033[32m 请访问： https://"$host"\033[0m"
