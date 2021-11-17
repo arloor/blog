4fa1ada19376ae858d4ffddca5c5cffcb751d07b
arloor <admin@arloor.com>
2021-11-17 21:09:53 +0800

    commit @arloor 2021年11月17日 星期三 21时09分53秒 CST

--- deploy.sh
+++ deploy.sh
@@ -26,20 +26,17 @@ else
     echo -e "\033[32m 推送失败 \033[0m"
 fi
 
-[ ! -d /tmp/arloor.github.io ] && echo "blog deploy no exist" && {
-        git clone https://github.com/arloor/arloor.github.io.git /tmp/arloor.github.io
-} || {
-        echo "blog deploy exists. git pull....";
-        cd /tmp/arloor.github.io
-        git pull;
-}
-cd $dir
+
+
+rm -rf /tmp/arloor.github.io
 hugo -d /tmp/arloor.github.io
 
 cd /tmp/arloor.github.io
+git init
 git add .
-git commit -m "$msg"
-git push -f
+git commit -m "init"
+git remote add origin https://github.com/arloor/arloor.github.io.git
+git push origin master -f
 cd $dir
 
 
