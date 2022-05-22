9851e0773386dddab7d0f6dedf40144691b97631
arloor <admin@arloor.com>
2022-05-22 15:31:02 +0800

    commit @arloor Sun May 22 15:31:01 CST 2022

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ function githubio() {
 git pull && git add . && git commit -m "$msg" && git push && {
   for host in $hosts; do
     ssh root@${host} -t "
-            wget "https://www.arloor.com/tarloor.sh" -O /usr/local/bin/tarloor
+            # wget "https://www.arloor.com/tarloor.sh" -O /usr/local/bin/tarloor
             bash tarloor 1 #使用代理: bash tarloor 1
             "
     echo -e "\033[32m 请访问： https://"${host}"\033[0m"
