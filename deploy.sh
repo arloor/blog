d2b7f7997ef7f48a9aeeb7365b3bdda99038a249
arloor <admin@arloor.com>
2022-06-03 13:00:25 +0800

    commit @arloor Fri Jun  3 13:00:23 CST 2022

--- deploy.sh
+++ deploy.sh
@@ -17,17 +17,16 @@ function githubio() {
   cd $dir
 }
 
-git pull && git add . && git commit -m "$msg" && git push && {
-  for host in $hosts; do
-    ssh root@${host} -t "
+git pull && git add . && git commit -m "$msg" && git push || {
+  echo -e "\033[32m 推送失败 \033[0m"
+}
+
+for host in $hosts; do
+  ssh root@${host} -t "
             # wget "https://www.arloor.com/tarloor.sh" -O /usr/local/bin/tarloor
             bash tarloor 1 #使用代理: bash tarloor 1
             "
-    echo -e "\033[32m 请访问： https://"${host}"\033[0m"
-  done
-} && {
-  #   githubio
-  echo
-} || {
-  echo -e "\033[32m 推送失败 \033[0m"
-}
+  echo -e "\033[32m 请访问： https://"${host}"\033[0m"
+done
+
+# githubio
