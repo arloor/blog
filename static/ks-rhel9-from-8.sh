db181ca829c89637b7cc40820375fb3be93c9431
arloor <admin@arloor.com>
2023-07-12 10:21:38 +0800

    commit @arloor 2023年 7月12日 星期三 10时21分37秒 CST

--- static/ks-rhel9-from-8.sh
+++ static/ks-rhel9-from-8.sh
@@ -7,8 +7,6 @@ curl -k  "${init_url}" -o '/boot/initrd.img'
 curl -k  "${kernel_url}" -o '/boot/vmlinuz'
 machineId=`cat /etc/machine-id`
 rm -rf /boot/loader/entries/${machineId}-vmlinuz*
-grubby --add-kernel=/boot/vmlinuz --initrd=/boot/initrd.img  --title="reinstall"  --args="ip=dhcp inst.ks=http://199.180.115.74/ks.cfg"
-cat /boot/loader/entries/${machineId}-vmlinuz.conf
-
-[[ -f  /boot/grub2/grubenv ]] && sed -i 's/saved_entry.*/saved_entry '${machineId}'-vmlinuz/g' /boot/grub2/grubenv;
+grubby --add-kernel=/boot/vmlinuz  --make-default --initrd=/boot/initrd.img  --title="reinstall"  --args="ip=dhcp inst.ks=http://199.180.115.74/ks.cfg"
+grep saved_entry /boot/grub2/grubenv
 echo reboot to install
